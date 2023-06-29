6d2bfe069ac1f0ca491189a7e94e36a9c57cea7a
menem <menem@DESKTOP-8V5FLBK>
2023-06-28 23:54:35 -0300

    Arreglado

--- pryVentasVerduleros/Program.cs
+++ pryVentasVerduleros/Program.cs
@@ -16,7 +16,7 @@ namespace pryVentasVerduleros
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new fMain());
         }
     }
 }
